cee6eabaacb374b683ee652433458637d2835903
hosono <hosono1985@gmail.com>
2020-09-11 11:07:38 +0900

    add styled-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import './App.css';
-
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Reset } from 'styled-reset';
 
 import { TPost } from './adapter';
 import Header from './components/Header';
@@ -14,14 +13,17 @@ const slug: TPost['id'] = 'id';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Top} />
-        <Route exact path="/edit" component={Edit} />
-        <Route exact path={`/:${slug}`} component={SinglePost} />
-      </Switch>
-    </Router>
+    <>
+      <Reset />
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Top} />
+          <Route exact path="/edit" component={Edit} />
+          <Route exact path={`/:${slug}`} component={SinglePost} />
+        </Switch>
+      </Router>
+    </>
   );
 };
 
