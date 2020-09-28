18646e5ef77efd41ffe2aaec35710625a703333e
hosonokotaro <hosono1985@gmail.com>
2020-09-28 19:03:25 +0900

    move auth component

---
+++ src/components/Edit/hooks/useAuth.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+
+import firebase from '../../../adapter';
+
+const useAuth = (): {
+  user: firebase.User | null | undefined;
+  login: () => void;
+  logout: () => void;
+} => {
+  const [user, setUser] = useState<firebase.User | null>();
+
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      setUser(user);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const login = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithRedirect(provider);
+  };
+
+  const logout = () => {
+    firebase.auth().signOut();
+  };
+
+  return {
+    user,
+    login,
+    logout,
+  };
+};
+
+export default useAuth;
