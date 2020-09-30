dc3f6b6d20563b864e06a5cbe33395656cee1cdb
hosonokotaro <hosono1985@gmail.com>
2020-09-30 15:13:31 +0900

    wip

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,21 @@
+// / <reference types="cypress" />
+
 describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(false);
+  it('ページを確認する', () => {
+    cy.visit('https://example.cypress.io/');
+
+    // 要素を検索する
+    // cy.contains('within');
+
+    // 要素をクリックする
+    cy.contains('type').click();
+
+    // URLを確認する（部分一致）
+    cy.url().should('include', '/commands/actions');
+
+    // formへ入力し、その内容を確認する
+    cy.get('.action-email')
+      .type('fake@email.com')
+      .should('have.value', 'fake@email.com');
   });
 });
