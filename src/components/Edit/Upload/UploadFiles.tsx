e5ebbef5596d04da62328382092e7f8db70babfb
hosono <hosono1985@gmail.com>
2020-10-08 19:32:49 +0900

    wip

--- src/components/Edit/Upload/UploadFiles.tsx
+++ src/components/Edit/Upload/UploadFiles.tsx
@@ -1,8 +1,13 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
+
+import { publicImages } from '../../../adapter';
 
 const UploadFiles: React.FC<{ uploadPath: string }> = (props) => {
   const filepathRef = useRef<HTMLInputElement>(null);
+
+  // 画像のフルパスと、画像のファイル名のリストが必要
+  const [imagePaths, setImagePaths] = useState<string[]>([]);
 
   const copyClipboard = () => {
     if (filepathRef.current) {
@@ -11,20 +16,79 @@ const UploadFiles: React.FC<{ uploadPath: string }> = (props) => {
     }
   };
 
+  const deleteImage = (imagePath: string) => {
+    const deleteConfirm = confirm('削除します');
+
+    if (!deleteConfirm) {
+      return false;
+    }
+
+    // const fetchPublicImages = () =>
+    //   publicImages.child(`${props.uploadPath}/${imagepath}`);
+
+    // desertRef.delete().then(
+  };
+
+  useEffect(() => {
+    const tmpImagePaths: string[] = [];
+
+    const fetchPublicImages = () =>
+      publicImages
+        .child(`${props.uploadPath}`)
+        .listAll()
+        .then(({ items }) => {
+          items.map((item) => {
+            item.getDownloadURL().then((url) => {
+              tmpImagePaths.push(url);
+            });
+          });
+
+          setImagePaths(tmpImagePaths);
+        });
+
+    fetchPublicImages();
+  }, []);
+
   return (
     <>
       <div>画像一覧</div>
-      <div>
-        <input ref={filepathRef} type="text" defaultValue="filepath" />
-        <button onClick={copyClipboard}>
-          画像パスをクリップボードにコピーする
-        </button>
-        <div>
-          <img src="http://placehold.jp/150x150.png" />
-        </div>
-      </div>
+      <StyledImagePaths>
+        {imagePaths.map((item, index) => {
+          return (
+            <StyledItem key={index}>
+              <input ref={filepathRef} type="text" defaultValue={item} />
+              <button onClick={copyClipboard}>
+                画像パスをクリップボードにコピーする
+              </button>
+              <button onClick={() => deleteImage(item)}>画像を削除する</button>
+              <StyledImgWrap>
+                <StyledImg src={item} />
+              </StyledImgWrap>
+            </StyledItem>
+          );
+        })}
+      </StyledImagePaths>
     </>
   );
 };
 
 export default UploadFiles;
+
+const StyledImagePaths = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const StyledItem = styled.div`
+  width: 50%;
+`;
+
+const StyledImgWrap = styled.div`
+  text-align: center;
+  background-color: #eee;
+`;
+
+const StyledImg = styled.img`
+  max-width: 100%;
+  max-height: 300px;
+`;
