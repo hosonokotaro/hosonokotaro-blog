db1cb843f3c290c6d19c9d477e53c243adebb6ee
hosono kotaro <hosono1985@gmail.com>
2020-12-26 16:53:54 +0900

    fix: change css style library

--- src/components/Edit/styledIndex.ts
+++ src/components/Edit/styledIndex.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import { styled } from '@linaria/react';
 
 export const StyledLogin = styled.div`
   max-width: 1000px;
--- src/components/Edit/styledNewPost.ts
+++ src/components/Edit/styledNewPost.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import { styled } from '@linaria/react';
 
 export const StyledSection = styled.section`
   max-width: 1000px;
