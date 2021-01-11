f2da73b6b20aa182907a73416f81648c22dd67e0
hosonokotaro <hosono1985@gmail.com>
2021-01-11 11:44:10 +0900

    fix: 共通 util 追加による不要コードの削除

--- src/components/Footer.test.tsx
+++ src/components/Footer.test.tsx
@@ -1,27 +1,14 @@
 import '@testing-library/jest-dom/extend-expect';
 
-import { render, screen } from '@testing-library/react';
 import React from 'react';
 
+import { render, screen } from '../testUtil';
+import getYear from '../utility/getYear';
 import Footer from './Footer';
-
-jest.mock('@linaria/react', () => {
-  const styled = (tag: string | number | symbol) => {
-    return jest.fn(() => tag.toString());
-  };
-
-  return {
-    styled: new Proxy(styled, {
-      get(o, prop) {
-        return o(prop);
-      },
-    }),
-  };
-});
 
 it('it should render: Footer', () => {
   render(<Footer />);
   expect(screen.getByRole('contentinfo')).toHaveTextContent(
-    '© 2021 HOSONOKOTARO Tech Blog'
+    `© ${getYear()} HOSONOKOTARO Tech Blog`
   );
 });
