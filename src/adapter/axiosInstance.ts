74e4046f0cf65ffc4c350686b45c2440ac19af30
hosono kotaro <hosono1985@gmail.com>
2021-01-23 14:56:09 +0900

    fix: add dev mode, dev url (#100)

--- src/adapter/axiosInstance.ts
+++ src/adapter/axiosInstance.ts
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+// NOTE: dev, prod で問い合わせ先が変更になる
+const devMode = process.env.NODE_ENV === 'development' ?? false;
+
 const axiosInstance = axios.create({
-  baseURL: 'https://asia-northeast1-hosonokotaro-blog.cloudfunctions.net/api',
+  baseURL: devMode
+    ? 'http://localhost:5001/hosonokotaro-blog/asia-northeast1/api'
+    : 'https://asia-northeast1-hosonokotaro-blog.cloudfunctions.net/api',
   headers: { 'Content-Type': 'application/json' },
   responseType: 'json',
 });
