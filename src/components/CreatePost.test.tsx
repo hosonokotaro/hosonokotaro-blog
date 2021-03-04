36195ebd28c8dccf6a725e5fe8fb2b24206123f2
hosonokotaro <hosono1985@gmail.com>
2021-03-05 00:34:08 +0900

    feat: add CreatePost test

---
+++ src/components/CreatePost.test.tsx
@@ -0,0 +1,42 @@
+import '@testing-library/jest-dom/extend-expect';
+
+import React from 'react';
+
+import { render, screen } from '~/testUtil';
+
+import CreatePost from './CreatePost';
+
+const handleSubmit = jest.fn();
+const onTitleChanged = jest.fn();
+
+it('it should render: CreatePost の input area に入れた文字が表示される', () => {
+  const title = 'test title';
+  const canSaveNewPost = true;
+
+  render(
+    <CreatePost
+      title={title}
+      handleSubmit={handleSubmit}
+      onTitleChanged={onTitleChanged}
+      canSaveNewPost={canSaveNewPost}
+    />
+  );
+
+  expect(screen.getByRole('textbox').getAttribute('value')).toEqual(title);
+});
+
+it('it should render: CreatePost の input area に入れた文字が表示されない', () => {
+  const title = '';
+  const canSaveNewPost = false;
+
+  render(
+    <CreatePost
+      title={title}
+      handleSubmit={handleSubmit}
+      onTitleChanged={onTitleChanged}
+      canSaveNewPost={canSaveNewPost}
+    />
+  );
+
+  expect(screen.getByRole('textbox').getAttribute('value')).toEqual(title);
+});
