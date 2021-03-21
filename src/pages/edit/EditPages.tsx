156a27b05128e52dc8c9b00a2028055cd9551df6
hosonokotaro <hosono1985@gmail.com>
2021-03-21 21:34:05 +0900

    fix: historyApiFallback

--- src/pages/edit/EditPages.tsx
+++ src/pages/edit/EditPages.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import SinglePost from '~/pages/SinglePost';
-import Top from '~/pages/Top';
-
 import Edit from './Edit';
 import EditPost from './EditPost';
 
 const EditPages: React.FC = () => {
   return (
     <Switch>
-      <Route exact path="/" component={Top} />
       <Route exact path="/edit" component={Edit} />
-      <Route exact path="/:id" component={SinglePost} />
       <Route exact path="/edit/:id" component={EditPost} />
     </Switch>
   );
