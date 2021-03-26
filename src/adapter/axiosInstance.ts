6cc6ba786f25186e286c8157948f83db790247d8
hosono kotaro <hosono1985@gmail.com>
2021-03-26 14:53:50 +0900

    feat: Add auth connect

--- src/adapter/axiosInstance.ts
+++ src/adapter/axiosInstance.ts
@@ -1,14 +1,42 @@
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 
-// NOTE: dev, prod で問い合わせ先が変更になる
-const devMode = process.env.NODE_ENV === 'development' ?? false;
+import { Auth } from './firebase';
+
+// NOTE: dev: local, prod: cloud functions
+const baseURL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:5001/hosonokotaro-blog/asia-northeast1/api'
+    : 'https://asia-northeast1-hosonokotaro-blog.cloudfunctions.net/api';
 
 const axiosInstance = axios.create({
-  baseURL: devMode
-    ? 'http://localhost:5001/hosonokotaro-blog/asia-northeast1/api'
-    : 'https://asia-northeast1-hosonokotaro-blog.cloudfunctions.net/api',
+  baseURL,
   headers: { 'Content-Type': 'application/json' },
   responseType: 'json',
 });
 
 export default axiosInstance;
+
+// NOTE: Edit 画面認証
+export const axiosAuthenticatedInstance = (): AxiosInstance => {
+  let bearerToken = '';
+
+  if (Auth.currentUser) {
+    Auth.currentUser
+      .getIdToken(true)
+      .then((idToken) => {
+        bearerToken = idToken;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  return axios.create({
+    baseURL,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${bearerToken}`,
+    },
+    responseType: 'json',
+  });
+};
