df26393be60392b3be2a08b787f449f86b129a4d
hosono kotaro <hosono1985@gmail.com>
2021-08-16 21:35:24 +0900

    fix: change global style import

--- src/pages/edit/App.tsx
+++ src/pages/edit/App.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import Footer from '@/atoms/Footer';
 import Header from '@/organisms/Header';
-import GlobalStyle from '~/pages/GlobalStyle';
 import getDate from '~/utility/getDate';
 
 import EditRouter from './Router';
@@ -15,7 +14,6 @@ const App: React.FC = () => {
       <Helmet>
         <title>Edit | Tech Blog | WEB DEVELOPER HOSONO KOTARO</title>
       </Helmet>
-      <GlobalStyle />
       <Router>
         <Header linkPath="/edit" />
         <EditRouter />
