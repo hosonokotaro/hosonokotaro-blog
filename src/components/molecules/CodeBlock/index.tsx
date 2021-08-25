0a1af5666970d0a181528f7a874535e5e28342b3
hosono kotaro <hosono1985@gmail.com>
2021-08-26 00:14:27 +0900

    fix: Refactoring CodeBlock component

--- src/components/molecules/CodeBlock/index.tsx
+++ src/components/molecules/CodeBlock/index.tsx
@@ -3,29 +3,28 @@ import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
 import tsx from 'react-syntax-highlighter/dist/cjs/languages/prism/tsx';
 import syntaxStyle from 'react-syntax-highlighter/dist/cjs/styles/prism/tomorrow';
 
-// FIXME: 呼び出し方を修正したい
 SyntaxHighlighter.registerLanguage('tsx', tsx);
 
-// NOTE: Syntax highlighter は value という props 名を受け入れる？
+// NOTE: Syntax highlighter は value という props が必須
 interface Props {
   value: string;
   language: string;
 }
 
 const CodeBlock: React.FC<Props> = ({ value, language }) => {
-  if (value === undefined) {
-    return null;
-  }
-
   return (
-    <SyntaxHighlighter
-      language={language}
-      style={syntaxStyle}
-      role="img"
-      aria-label="コードスニペットによる説明"
-    >
-      {value}
-    </SyntaxHighlighter>
+    <>
+      {value && (
+        <SyntaxHighlighter
+          language={language}
+          style={syntaxStyle}
+          role="img"
+          aria-label="コードスニペットによる説明"
+        >
+          {value}
+        </SyntaxHighlighter>
+      )}
+    </>
   );
 };
 
