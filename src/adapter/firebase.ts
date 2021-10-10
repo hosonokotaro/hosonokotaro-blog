ab41292a67c52aa0d46d4911f72aed749a8bff24
hosono kotaro <hosono1985@gmail.com>
2021-10-10 23:10:18 +0900

    fix: Remove firebase compat

--- src/adapter/firebase.ts
+++ src/adapter/firebase.ts
@@ -1,8 +1,4 @@
-import 'firebase/compat/auth';
-import 'firebase/compat/storage';
-
 import { initializeApp } from 'firebase/app';
-import firebase from 'firebase/compat/app';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBsC6ZAinkt3n3fB0B-Yo1UMqwqaoCzi4s',
@@ -13,7 +9,5 @@ const firebaseConfig = {
   messagingSenderId: '23911354523',
   appId: '1:23911354523:web:56bec2eb2bf7d1b712f63e',
 };
-
-firebase.initializeApp(firebaseConfig);
 
 export const firebaseApp = initializeApp(firebaseConfig);
