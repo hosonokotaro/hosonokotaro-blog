59db45302e5cf791b7fd143afc53d159ae6cb5ec
hosono kotaro <hosono1985@gmail.com>
2021-11-10 13:44:37 +0900

    feat: Add Anchor component

---
+++ src/components/atoms/Anchor/index.tsx
@@ -0,0 +1,22 @@
+import React, { VFC } from 'react';
+
+import { StyledAnchor, StyledLink } from './styledIndex';
+
+interface Props {
+  children: React.ReactNode;
+  linkPath: string;
+}
+
+const Anchor: VFC<Props> = ({ children, linkPath }) => {
+  if (linkPath.match(/^(http|https):\/\//)) {
+    return (
+      <StyledAnchor href={linkPath} target="_blank" rel="noopener noreferrer">
+        {children}
+      </StyledAnchor>
+    );
+  }
+
+  return <StyledLink to={linkPath}>{children}</StyledLink>;
+};
+
+export default Anchor;
